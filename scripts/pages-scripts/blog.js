5c363387bc309cfd8c99eb9db010c91740b56525
MaskeyDude <maskey.dude@gmail.com>
2025-01-20 06:05:58 +0300

    writing blog 4

--- scripts/pages-scripts/blog.js
+++ scripts/pages-scripts/blog.js
@@ -4,21 +4,21 @@ const Card = [
         id: 2,
         title: "THE REASON TO LIVE",
         link: "../../pages/blogs/blah-2.html",
-        time: "MON, 05 AUG AT 06:30 PM"
+        time: "MON, 05 AUG 24 AT 06:30 PM"
     },
 
     {
         id: 1,
         title: "ThE CARE, NoW A DIsTURBANCE...?",
         link: "../../pages/blogs/blah-1.html",
-        time: "WED, 10 JUL AT 07:26 AM"
+        time: "WED, 10 JUL 24 AT 07:26 AM"
     },
 
     {
         id: 0,
         title: "My fiRsT blog i gUEss?",
         link: "../../pages/blogs/blah-0.html",
-        time: "FRI, 05 JUL AT 10:18 AM"
+        time: "FRI, 05 JUL 24 AT 10:18 AM"
     },
 
 ];
